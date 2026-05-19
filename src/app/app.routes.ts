1abf7cfb81c8e654ab1371354f17068286113dd5
Ashna Sharma <ashnasharma@example.com>
2026-05-19 22:57:26 +0530

    Updated hero section and responsiveness

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,33 @@
-import { Routes,RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Packages } from './pages/packages/packages';
 import { Contact } from './pages/contact/contact';
 import { About } from './pages/about/about';
-import { CategoryPackages } from './pages/category-packages/category-packages';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full'},
+    {
+        path: '',
+        component: Home
+    },
 
-    { path: 'home', component: Home},
-    { path:'packages', component:Packages},
-    // { path: 'packages/:category', component: CategoryPackages },
-    { path: 'about', component: About},
-    { path: 'contact', component:Contact},
+    {
+        path: 'packages',
+        component: Packages
+    },
 
-    { path: '**', redirectTo: 'home'}
-    
+    {
+        path: 'about',
+        component: About
+    },
+
+    {
+        path: 'contact',
+        component: Contact
+    },
+
+    {
+        path: '**',
+        redirectTo: ''
+    }
+
 ];
